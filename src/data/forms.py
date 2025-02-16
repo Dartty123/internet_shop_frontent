15335bbb6d73f202f2fdcd94fe16b8b59df6735d
Dartty123 <dariyatamanuk@icloud.com>
2025-02-16 11:04:15 +0200

    save

--- src/data/forms.py
+++ src/data/forms.py
@@ -2,8 +2,15 @@ from flask_wtf import FlaskForm
 import wtforms
 
 
-class SignUp(FlaskForm):
+class SignUpForm(FlaskForm):
     first_name = wtforms.StringField("Ім'я")
     last_name = wtforms.StringField("Призвіще")
     email = wtforms.EmailField("Електрона пошта", validators=[wtforms.validators.Email(), wtforms.validators.DataRequired()])
-    password = wtforms.PasswordField("Пароль"), validators=[wtforms.validators.length(6)]
+    password = wtforms.PasswordField("Пароль", validators=[wtforms.validators.length(6)])
+    submit = wtforms.SubmitField("Зареєструватись")
+
+
+class LoginForm(FlaskForm):
+    email = wtforms.EmailField("Електрона пошта",  validators=[wtforms.validators.Email(), wtforms.validators.DataRequired()])
+    password = wtforms.PasswordField("Пароль", validators=[wtforms.validators.length(6)])
+    submit = wtforms.SubmitField("Увійти")
